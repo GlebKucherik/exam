324da5796861041c54986e980fa2b4aa442e9db6
GlebKucherik <95340898+GlebKucherik@users.noreply.github.com>
2023-04-26 09:53:19 +0300

    создал проект и добавил класс driver

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+
+
+namespace ThePracticeExam.Program
+{
+    class Program
+    {
+        public class Driver
+        {
+            public Driver(int length)
+            {
+                _Autos = new Auto[length];
+            }
+            public Driver(Auto[] Autos)
+            {
+                _Autos = Autos;
+            }
+
+            private Auto[] _Autos;
+
+            public int Legnth => _Autos.Length;
+
+            public Auto this[int i]
+            {
+                get => _Autos[i];
+                set => _Autos[i] = value;
+            }
+           
